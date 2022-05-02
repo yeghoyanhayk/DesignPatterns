7aeeb611e4837f35dec494f53da728f777374719
development.yeghoyanhayk@gmail.com <y3gh0y4n>
2022-05-03 01:27:24 +0400

    Singleton

---
+++ Singleton/SingletonExample.cs
@@ -0,0 +1,51 @@
+namespace DesignPatterns.Singleton;
+
+//Eager initialization of singleton
+public class SingletonEager
+{
+    private static SingletonEager instance = new SingletonEager();
+
+    private SingletonEager()
+    {
+    }
+
+    public static SingletonEager GetInstance => instance;
+}
+
+////lazy initialization of singleton
+public class SingletonLazy
+{
+    private static SingletonLazy? _instance = null;
+
+    private SingletonLazy()
+    {
+    }
+
+    public static SingletonLazy? GetInstance
+    {
+        get { return _instance ??= new SingletonLazy(); }
+    }
+}
+
+////Thread-safe initialization of singleton
+public class SingletonThreadSafe
+{
+    private static SingletonThreadSafe? _instance = null;
+
+    private SingletonThreadSafe()
+    {
+    }
+
+    private static readonly object LockThis = new object();
+
+    public static SingletonThreadSafe? GetInstance
+    {
+        get
+        {
+            lock (LockThis)
+            {
+                return _instance ??= new SingletonThreadSafe();
+            }
+        }
+    }
+}
